7e85c8a3d258f531fce7e8316c67c55638fa32e0
Sophonias D Gurmu <mail2sophi@gmail.com>
2015-03-07 22:00:35 -0500

    Added Tables and google prettifY

---
+++ IStackTraceDatabase/Tables/Article.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Article]
+(
+	[ArticleId] INT NOT NULL PRIMARY KEY, 
+    [Description] VARCHAR(300) NULL, 
+    [Text] VARCHAR(MAX) NULL, 
+    [PublishedBy] INT NOT NULL, 
+    [PublishedOn] TIMESTAMP NOT NULL, 
+    [Title] VARCHAR(200) NOT NULL 
+)
---
+++ IStackTraceDatabase/Tables/Tag.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Tag]
+(
+	[TagId] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [Description] VARCHAR(200) NULL
+)
